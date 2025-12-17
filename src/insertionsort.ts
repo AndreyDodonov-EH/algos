0d04602f8ed5a2380b5fdf59740b853f91994d0d
Andrey Dodonov <Andrey.Dodonov@endress.com>
2025-12-17 15:20:22 +0100

    Cleanup insertion sort

--- src/insertionsort.ts
+++ src/insertionsort.ts
@@ -1,17 +1,4 @@
-function insert(B: number[], x: number) {
-    B.push(x);
-    for (let i = B.length-2;i>=0;i--) {
-        if (B[i]>x){
-            [B[i], B[i+1]] = [B[i+1], B[i]];
-        } else {
-            break;
-        }
-    }
-    return B;
-}
-
-
-function insertionsort_swap(A: number[]) {
+function insertionsort_swap_for(A: number[]) {
     for (let i=1;i<A.length;i++) { 
         for (let j=i-1;j>=0;j--) {
             if (A[j]>A[j+1]) {
@@ -23,24 +10,28 @@ function insertionsort_swap(A: number[]) {
     }
 }
 
-// ToDo: fix
-function insertionsort_shift(A:number[]) {
+function insertionsort_shift_for(A:number[]) {
     for (let i=1;i<A.length;i++) {
         const current = A[i];
-        for(let j=i-1;j>=0;j--) {
-            if (A[j] < current) {
-                A[j] = current
+        let j=i-1;
+        for(;j>=0;j--) {
+            if (A[j] <= current) {
                 break;
             }
             A[j+1] = A[j]; // shift
         }
+        A[j+1] = current;
     }
 }
 
-function test_insertionsort() {
-    let A: number[] = [2, 8, 7, 1];
-    insertionsort_shift(A);
-    console.log(A);
+function insertionsort_shift_while(A: number[]) {
+    for (let i=1; i<A.length; i++) {
+        const current = A[i];
+        let j=i-1;
+        while(j>=0 && A[j]>current) {
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = current;
+    }
 }
-
-test_insertionsort();
