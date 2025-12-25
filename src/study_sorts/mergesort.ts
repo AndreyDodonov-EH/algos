ca0c1573c264e99a0199798b8a062b7705afc4c7
Andrey Dodonov <Andrey.Dodonov@endress.com>
2025-12-25 17:45:49 +0100

    Fix in-place merge!

--- src/study_sorts/mergesort.ts
+++ src/study_sorts/mergesort.ts
@@ -7,68 +7,68 @@ function swap(A: number[], i: number, j: number) {
 function merge_rec(A: number[], a: number, p: number, r: number) {
     let i = a;
     let j = p;
-    for (; i < p; i++) {
-        if (A[i]<A[j]) {
+    for (; i < p && j < r; i++) {
+        if (A[i] <= A[j]) {
             continue;
         }
         swap(A, i, j);
-        if (A[j] > A[j+1]) {
+        if (j+1 < r && A[j] > A[j + 1]) {
             j++;
         }
-    }
-    if (i==j) {
-        return;
-    }
-    merge_rec(A, a, p, j-1);
-    merge_rec(A, a, j, r);
-}
-
-function merge(A: number[], a: number, p: number, r: number) {
-    let B: number[] = new Array(r-p);
-    let i = a;
-    let j = p; 
-    let k = 0;
-    for (;i<p&&j<=r;k++) {
-        if (A[i] < A[j]) {
-            B[k] = A[i++];
-        } else {
-            B[k] = A[j++];
-        }
-    }
-    for (;i<p;i++,k++) {
-        B[k] = A[i];
-    }
-    for (;j<r;j++,k++) {
-        B[k] = A[j];
-    }
-    for (let i=0, j=a;i<B.length;i++,j++) {
-        A[j] = B[i]; 
+     }
+    if (p < j) {
+        merge_rec(A, a, p, j);
+        merge_rec(A, p, j, r);
     }
 }
 
 
 function mergesort_body(A: number[], p: number, r: number) {
-    if (p>=r) {
+    if (r - p < 2) {
         return;
     }
     const mid = p + Math.floor((r-p)/2);
     mergesort_body(A, p, mid);
-    mergesort_body(A, mid+1, r);
-    merge_rec(A, p, mid+1, r);
+    mergesort_body(A, mid, r);
+    merge_rec(A, p, mid, r);
 }
 
 
 
 function mergesort(A: number[]) {
-    mergesort_body(A, 0, A.length - 1);
+    mergesort_body(A, 0, A.length);
+}
+
+function randomIntArray(
+    length: number,
+    min: number,
+    max: number // inclusive
+  ): number[] {
+    return Array.from({ length }, () =>
+      Math.floor(min + Math.random() * (max - min + 1))
+    );
+  }
+
+  function isSorted(a: readonly number[]): boolean {
+  for (let i = 1; i < a.length; i++) {
+    if (a[i - 1] > a[i]) return false;
+  }
+  return true;
 }
 
 function test_merge() {
-    let A: number[] = [2, 3, 8, 19, 50, 100, 1, 5, 7, 11, 20, 37, 10, 25];
+    // let A: number[] = [2, 3, 8, 19, 50, 100, 1, 5, 7, 11, 20, 37, 10, 25];
     // let A: number[] = [2,8, 19, 3, 10];
-    // merge_rec(A, 0, 2, A.length - 1);
+    for (let i = 0; i < 1; i++) {
+    let A: number[] = randomIntArray(10000, 0, 1000000);
+    // let A: number[] = [6, 9, 4, 6, 2, 7, 5, 1];
+    // console.log(A);
     mergesort(A);
-    console.log(A);
+    if (!isSorted(A)) {
+        console.error("Array is not sorted!");
+    }
+    // console.log(A);
+    }
 }
 
 test_merge();
