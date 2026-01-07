6f280eb0e94e005b2a3d739628269b3c90a8045a
Andrey Dodonov <Andrey.Dodonov@endress.com>
2026-01-07 12:55:18 +0100

    Structure quicksort variants

---
+++ src/study_sorts/quicksort/lomuto.ts
@@ -0,0 +1,41 @@
+import { log } from "../../helpers/log"
+import { test_sort } from "../../helpers/test_harness";
+
+function swap(A: number[], i: number, j: number) {
+    const tmp = A[i];
+    A[i] = A[j];
+    A[j] = tmp;
+}
+
+let total_pivot_size = 0;
+
+function partition_lomuto(A: number[], l: number, r: number): number {
+    total_pivot_size++;
+    // select last value as separator
+    let idx_of_first_bigger = l;
+    for(let i=l;i<r-1;i++) {
+        if (A[i] < A[r-1]) {
+            swap(A, i, idx_of_first_bigger++);
+        }
+    }
+    swap(A, idx_of_first_bigger, r-1);
+    return idx_of_first_bigger;
+}
+
+function quicksort_body(A: number[], l: number, r: number) {
+    if (r-l<=1) {
+        return;
+    }
+    // 1. partition into two parts sorted relative to each other
+    const pivot = partition_lomuto(A, l, r);
+    // 2. recurse into those parts
+    quicksort_body(A, l, pivot);
+    quicksort_body(A, pivot+1,r);
+}
+
+function quicksort(A: number[]) {
+    quicksort_body(A, 0, A.length);
+    log(`T: ${total_pivot_size}`);
+}
+
+test_sort(quicksort);
--- src/study_sorts/quicksort/quicksort_sedgewick.ts
+++ src/study_sorts/quicksort/quicksort_sedgewick.ts
@@ -1,14 +1,35 @@
 import { test_sort } from "../../helpers/test_harness";
 
+function swap(A:number[], i:number, j:number){
+    const tmp = A[i];
+    A[i] = A[j];
+    A[j] = tmp;
+}
+
+function partition_sedgewick_start(A:number[], l:number, r:number): [number, number] {
+    const pivotIdx = l;
+    const pivotValue = A[pivotIdx];
+    let i = l+1;
+    let j = r-1;
+    while(true) {
+        while(i<j && A[i] < pivotValue) i++;
+        while(j>i && A[j] > pivotValue) j--;
+        if (i>=j) { break; }
+        swap(A, i++, j--);
+    }
+    if(A[i] < A[pivotIdx]) swap(A, pivotIdx, i);
+    return [i,j]
+}
+
 /// go from two ends and swap if both are mismatched
 /// also added swap middle with end "ritual"
 /// a.k.a. Introsort partition
-function partition_sedgewick(A: number[], p: number, r: number):[number, number] {
-    const m = Math.floor((p+r)/2);
-    [A[m], A[r]] = [A[r], A[m]];
-    let x = A[r];
-    let i = p;
-    let j = r - 1;
+function partition_sedgewick_middle_plus_ritual(A: number[], l: number, r: number):[number, number] {
+    const m = l + Math.floor((r-l)/2);
+    swap(A,m,r-1);
+    let x = A[r-1];
+    let i = l;
+    let j = r - 2;
     while (true) {
         while (A[i] < x) i++;
         while (j > i && A[j] > x) {
@@ -17,22 +38,28 @@ function partition_sedgewick(A: number[], p: number, r: number):[number, number]
         if (i >= j) {
             break;
         }
-        [A[i], A[j]] = [A[j], A[i]];
-        i++; j--;
+        swap(A,i++,j--);
     }
-    [A[i], A[r]] = [A[r], A[i]];
+    swap(A,i,r-1);
     return [i, j];
 }
 
-function quicksort_body(A: number[], p: number, r: number) {
-    if (r<=p) return;
-    const pivots = partition_sedgewick(A, p, r);
-    quicksort_body(A, p, pivots[0]-1);
-    quicksort_body(A, pivots[0]+1, r);
+
+
+function quicksort_body(A:number[], l:number, r:number) {
+    if (r-l<=1) {
+        return;
+    }
+    // const pivot = partition_sedgewick_start(A, l, r);
+    // quicksort_body(A, l, pivot[0]);
+    // quicksort_body(A, pivot[0], r);
+    const pivot = partition_sedgewick_middle_plus_ritual(A, l, r);
+    quicksort_body(A, l, pivot[0]);
+    quicksort_body(A, pivot[0]+1, r);
 }
 
-export function quicksort(A: number[]) {
-    quicksort_body(A, 0, A.length - 1);
+function quicksort(A:number[]) {
+    quicksort_body(A, 0, A.length);
 }
 
 test_sort(quicksort);
